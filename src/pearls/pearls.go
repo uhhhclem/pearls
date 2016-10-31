771dd56f6d4b5e2a64f42f0fc462e928a5fbbb73
uhhhclem <rrossney@gmail.com>
2016-10-30 19:07:04 -0700

    Tests for GetBit and SetBit pass.

--- src/pearls/pearls.go
+++ src/pearls/pearls.go
@@ -1,5 +1,9 @@
 package pearls
 
+import (
+	"log"
+)
+
 const maxNum = 100000
 
 type Bitmap []byte
@@ -9,10 +13,25 @@ func NewBitmap() Bitmap {
 	return Bitmap(b)
 }
 
-func (b *Bitmap) SetBit(_ int) {
-
+func (b Bitmap) SetBit(n int) {
+	if n < 0 || n > maxNum {
+		log.Fatalf("SetBit:  invalid argument: %d", n)
+	}
+	i, mask := getIndexAndMask(n)
+	b[i] = b[i] | mask
 }
 
-func (b *Bitmap) GetBit(_ int) bool {
-	return false
+func (b Bitmap) GetBit(n int) bool {
+	if n < 0 || n > maxNum {
+		log.Fatalf("GetBit:  invalid argument: %d", n)
+	}
+	i, mask := getIndexAndMask(n)
+	return (b[i] & mask != 0)
+}
+
+func getIndexAndMask(n int) (int, byte) {
+	i := n / 8
+	bit := byte(n % 8)
+	mask := byte(1 << bit)
+	return i, mask	
 }
